b26ecff5bd9742b48cb3df69e71292580119823c
Dmitry Machin <dmac@ciklum.com>
2016-06-02 18:24:06 +0300

    more tests

--- Tests/Service/ActivityLog/ActivityLogFormatterTest.php
+++ Tests/Service/ActivityLog/ActivityLogFormatterTest.php
@@ -5,6 +5,7 @@ namespace Service\ActivityLog;
 
 use ActivityLogBundle\Entity\LogEntry;
 use ActivityLogBundle\Service\ActivityLog\ActivityLogFormatter;
+use ActivityLogBundle\Service\ActivityLog\EntityFormatter\UniversalFormatter;
 
 class ActivityLogFormatterTest extends \PHPUnit_Framework_TestCase
 {
@@ -36,4 +37,34 @@ class ActivityLogFormatterTest extends \PHPUnit_Framework_TestCase
         $this->assertArrayHasKey('message', $result[0]);
         $this->assertEquals('The entity <b>Name (ObjectClass)</b> was created.', $result[0]['message']);
     }
+
+    public function testCustomFormat()
+    {
+        $logger = $this->getMockBuilder('Psr\Log\LoggerInterface')
+            ->getMock();
+        $logger->method('warning')
+            ->willReturn($this->returnValue(null));
+
+        $em = $this->getMockBuilder('\Doctrine\ORM\EntityManager')
+            ->getMock();
+
+        $factory = new ActivityLogFormatter($logger, $em, 'ActivityLogBundle\Service\ActivityLog\EntityFormatter');
+        $logEntry = new LogEntry();
+        $logEntry->setOldData(['test' => 'test']);
+        $logEntry->setUsername('username');
+        $logEntry->setParentClass('AppBundle\Entity\ParentClass');
+        $logEntry->setAction('create');
+        $logEntry->setName('Name');
+        $logEntry->setParentId('parent-id');
+        $logEntry->setData(['test' => 'test1']);
+        $logEntry->setObjectClass('AppBundle\Entity\Custom');
+        $logEntry->setObjectId('object-id');
+        $logEntry->setVersion(2);
+        $result = $factory->format([$logEntry]);
+
+        $this->assertTrue(is_array($result[0]));
+        $this->assertArrayHasKey('message', $result[0]);
+        $this->assertEquals('The entity <b>Name (Custom)</b> was created.', $result[0]['message']);
+    }
+
 }
